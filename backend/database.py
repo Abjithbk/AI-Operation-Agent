5e3b69ca9ffbd9451973ba7fdaf786fe2cad513b
Abjithbk <bkabjith2@gmail.com>
2026-06-16 15:11:56 +0530

    Fix: added the database url checking case

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,8 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+if not DATABASE_URL:
+    raise ValueError('DATABASE_URL environment variable is not set')
 engine = create_engine(DATABASE_URL,pool_pre_ping=True,pool_recycle=300)
 
 SessionLocal = sessionmaker(autocommit = False,autoflush=False,bind=engine)
